88cc98d3dd93776c03d9bccb1742d3a986a87db9
xiaobing.zhang <xiaobing.zhang@intel.com>
2019-09-20 18:41:06 +0800

    add run script

---
+++ imagenet/run_cpu.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+CORES=`lscpu | grep Core | awk '{print $4}'`
+SOCKETS=`lscpu | grep Socket | awk '{print $2}'`
+TOTAL_CORES=`expr $CORES \* $SOCKETS`
+
+KMP_SETTING="KMP_AFFINITY=granularity=fine,compact,1,0"
+KMP_BLOCKTIME=1
+
+export OMP_NUM_THREADS=$TOTAL_CORES
+export $KMP_SETTING
+export KMP_BLOCKTIME=$KMP_BLOCKTIME
+
+echo -e "### using OMP_NUM_THREADS=$TOTAL_CORES"
+echo -e "### using $KMP_SETTING"
+echo -e "### using KMP_BLOCKTIME=$KMP_BLOCKTIME\n"
+
+python -u main.py -a resnet50 --mkldnn /lustre/dataset/imagenet/img_raw -b 128
+
+#python -u main.py -a resnext101_32x8d --mkldnn /lustre/dataset/imagenet/img_raw -b 32
